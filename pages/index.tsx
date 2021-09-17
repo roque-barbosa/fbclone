351125ad97bce1f6fde4abb4ba77298ad0f904b8
trucopa <roqejr1307@gmail.com>
2021-09-16 23:00:33 -0300

    steup tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,9 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Facebook Clone</title>
       </Head>
